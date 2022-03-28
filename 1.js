66a6236e0bde3e12b5464ab61019b1261ff2150d
Brad Slagle <btslagle@gmail.com>
2022-03-28 14:56:02 -0500

    Fix #2

--- 1.js
+++ 1.js
@@ -14,6 +14,7 @@ Note:
 
 function getVowels() {
     const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
     let vowels = ["a", "e", "i", "o", "u", "y"]
 
 
--- 2.js
+++ 2.js
@@ -8,16 +8,16 @@ Given an array containing the english alphabet, write code that returns an array
 
 function getAlphaNumericAlphabet() {
     const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    alphabet.push("&")
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
-    let alphaNumericAlphabet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ...alphabet]
+    let alphaNumericAlphabet = [ ...numbers , ...alphabet];
+    const alphaNumericAlphabetWithAmp = [...alphaNumericAlphabet, "&"];
+
+    return alphaNumericAlphabetWithAmp 
 
 
-
-    return alphaNumericAlphabet
-}
-
+console.log(getAlphaNumericAlphabet())
 module.exports = getAlphaNumericAlphabet
 
 /*
